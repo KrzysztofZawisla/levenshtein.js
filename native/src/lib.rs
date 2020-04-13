c6b31eff6db902a83410fc1da7dba6c1401699c8
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-04-13 12:52:25 +0200

    Fixed the matching mechanism

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -9,10 +9,21 @@ fn filter(mut cx: FunctionContext) -> JsResult<JsArray> {
     let distance = cx.argument::<JsNumber>(1)?.value() as usize;
     let collection = cx.argument::<JsArray>(2)?;
     let collection_as_vec: Vec<Handle<JsValue>> = collection.to_vec(&mut cx)?;
+    let input_chars_array: Vec<char> = input.chars().collect();
     let mut temporary_vec = vec![];
     for value in collection_as_vec.iter() {
+        let mut shortened_value = vec![];
         let value = value.downcast::<JsString>().or_throw(&mut cx)?.value();
-        let counted_distance = levenshtein(&input, &value);
+        let value_chars_array: Vec<char> = value.chars().collect();
+        for (i, _) in input_chars_array.iter().enumerate() {
+            if value_chars_array.len() > i {
+                shortened_value.push(value_chars_array[i]);
+            } else {
+                break;
+            }
+        }
+        let shortened_value: String = shortened_value.into_iter().collect();
+        let counted_distance = levenshtein(&input, &shortened_value);
         if counted_distance <= distance {
             temporary_vec.push(value);
         }
